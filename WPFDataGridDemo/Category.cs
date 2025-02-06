145d1a322f1c1c9e9eb33f5c9e0016b579572038
wangrun <1291223834@qq.com>
2025-02-06 15:49:31 +0800

    feat(project): Initial commits;
    1. 实现DataGrid中显示CheckBox列；
    2. 实现DataGrid中CheckBox列的Header显示CheckBox；
    3. 实现DataGrid中CheckBox列选中状态和Header中的CheckBox选中状态联动；

---
+++ WPFDataGridDemo/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDataGridDemo
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Product:INotifyPropertyChanged
+    {
+        private bool isSelected;
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set {
+                if (value != IsSelected)
+                {
+                    isSelected = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsSelected)));
+                }
+            }
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
---
+++ WPFDataGridDemo/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFDataGridDemo
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool? isAllSelected;
+
+        public bool? IsAllSelected
+        {
+            get { return isAllSelected; }
+            set
+            {
+                if (value != isAllSelected)
+                {
+                    isAllSelected = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAllSelected)));
+                }
+            }
+        }
+
+        public ObservableCollection<Product> Products { get; set; }
+        public ObservableCollection<Category> Categories { get; set; }
+        public ObservableCollection<string> MySources { get; set; }
+        public ICommand SelectAllCommand { get; }
+        public ICommand CellValueChangedCommand { get; }
+
+        public MainViewModel()
+        {
+            // 初始化分类数据
+            Categories = new ObservableCollection<Category>
+            {
+                new Category { Id = 1, Name = "Electronics" },
+                new Category { Id = 2, Name = "Clothing" },
+                new Category { Id = 3, Name = "Home Appliances" }
+            };
+
+            // 初始化产品数据
+            Products = new ObservableCollection<Product>
+            {
+                new Product { Name = "Laptop", Price = 999.99m, CategoryId = 1 },
+                new Product { Name = "Shirt", Price = 29.99m, CategoryId = 2 },
+                new Product { Name = "Washing Machine", Price = 499.99m, CategoryId = 3 }
+            };
+            foreach (var item in Products)
+            {
+                item.PropertyChanged += Item_PropertyChanged;
+            }
+            MySources = new ObservableCollection<string>()
+            {
+                "Laptop" ,
+                "Shirt" ,
+                "Washing Machine"
+            };
+            SelectAllCommand = new RelayCommand(SelectAll);
+            CellValueChangedCommand = new RelayCommand(CellValueChanged);
+            IsAllSelected = false;
+        }
+
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            UpdateHeaderCheckBoxState();
+        }
+
+        private void SelectAll(object obj)
+        {
+            if (obj is bool isSelected)
+            {
+                foreach (var item in Products)
+                {
+                    item.IsSelected = isSelected;
+                }
+            }
+        }
+        private void CellValueChanged(object obj)
+        {
+            UpdateHeaderCheckBoxState();
+        }
+        public void UpdateHeaderCheckBoxState()
+        {
+            var allChecked = Products.All(item => item.IsSelected == true);
+            var allUnchecked = Products.All(item => item.IsSelected == false);
+
+            IsAllSelected = allChecked ? true : (allUnchecked ? false : (bool?)null);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
